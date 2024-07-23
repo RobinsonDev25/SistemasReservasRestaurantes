1652a2012bfd8936ddfb4ae0db0720ecd433a43c
RobinsonZam025 <robinsonzambranos1@hotmail.com>
2024-07-23 16:25:34 -0500

    iniciosession

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InicioSeccionComponent } from './inicio-seccion/inicio-seccion.component';
+import { RegistroComponent } from './registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/inicio-seccion', pathMatch: 'full'},
+    {path: 'inicio-seccion', component: InicioSeccionComponent},
+    {path: 'registro', component: RegistroComponent},
+];
